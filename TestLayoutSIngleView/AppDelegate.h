ac65d6a27b230d4681c3667eb9632328f32013b0
John Stewart <john@johnstewart.com>
2017-12-03 17:15:45 -0600

    Initial Commit

---
+++ TestLayoutSIngleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLayoutSIngleView
+//
+//  Created by John Stewart on 12/3/17.
+//  Copyright © 2017 John Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLayoutSIngleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLayoutSIngleView
+//
+//  Created by John Stewart on 12/3/17.
+//  Copyright © 2017 John Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
